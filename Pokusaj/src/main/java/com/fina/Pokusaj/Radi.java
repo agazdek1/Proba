b2630f334fed30176655ade75b0a66a2ffb9cfe6
Antonio Gazdek <agazdek1@fina.hr>
2020-03-17 11:58:19 +0100

    linija7

--- Pokusaj/src/main/java/com/fina/Pokusaj/Radi.java
+++ Pokusaj/src/main/java/com/fina/Pokusaj/Radi.java
@@ -54,6 +54,7 @@ public class Radi extends UI {
 	
 	Grid<Protocol> grid5 = new Grid<Protocol>();
 	Grid<Protocol> grid6 = new Grid<Protocol>();
+	Grid<Protocol> grid7 = new Grid<Protocol>();
 
 	@Autowired
 	TodoLayout layout;
